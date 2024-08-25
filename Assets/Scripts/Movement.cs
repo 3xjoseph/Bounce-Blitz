bb6bdfb720d8ebb4e50dd660c1fcd624ce7762d7
Patrick Joseph Pronuevo <3xjoseph@gmail.com>
2024-08-25 20:24:51 +0800

    2. Added Movement
    Added movement script for the player.

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements.Experimental;
+
+public class Movement : MonoBehaviour
+{
+
+    [SerializeField] float moveSpeed; //Set the Speed for your character
+   
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float xValue = (Input.GetAxis("Horizontal") * Time.deltaTime) * moveSpeed;
+        float zValue = (Input.GetAxis("Vertical") * Time.deltaTime) * moveSpeed;
+       
+        transform.Translate(xValue, 0, zValue);
+    }
+}
